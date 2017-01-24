ac0c4278d6baa03c65968ce1ed2a18cdb5b28068
Yogendra <48yogen@gmail.com>
2017-01-24 08:41:34 +0545

    GetReport Function Added

--- ReportDemo01/Controllers/HomeController.cs
+++ ReportDemo01/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Reporting.WebForms;
+
 namespace ReportDemo01.Controllers
 {
     public class HomeController : Controller
@@ -11,6 +13,29 @@ namespace ReportDemo01.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult GetReport()
+        {
+            List<Home> homes = new List<Home>();
+
+            Warning[] warnings;
+            string mimeType;
+            string[] streamids;
+            string encoding;
+            string filenameExtension;
+
+            using (ShangrilaReportsEntities dc = new ShangrilaReportsEntities())
+            {
+                homes = dc.Homes.ToList();
+            }
+
+            var viewer = new ReportViewer();
+            viewer.LocalReport.ReportPath = @"Reports\RPDhito.rdlc";
+            ReportDataSource rdc = new ReportDataSource("MyDataset", homes);
+            viewer.LocalReport.DataSources.Add(rdc);
+            viewer.LocalReport.Refresh();
+            var bytes = viewer.LocalReport.Render("PDF", null, out mimeType, out encoding, out filenameExtension, out streamids, out warnings);
+            return new FileContentResult(bytes, mimeType);
         }
     }
 }
